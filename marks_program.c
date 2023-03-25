6e252e01aba51852e0905b7fa8d98bcb0dee8cb1
marksoulier <122393887+marksoulier@users.noreply.github.com>
2023-03-25 00:39:43 -0600

    mark beginning program

---
+++ marks_program.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define ROWS 3
+#define COLS 4
+
+int main() {
+    int array[ROWS][COLS] = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} };
+
+    printf("Array contents:\n");
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", array[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
